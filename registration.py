fa109e87611783223972807159e1fd255d173a6d
Taewan Park <swimingtw@gmail.com>
2019-02-07 20:55:44 +0900

    card id overlap check func upload

--- registration.py
+++ registration.py
@@ -19,6 +19,18 @@ def registration():
     print("등록하실 카드를 찍어 주세요.")
     card = nfctoid.scan_id()
 
+    ccheck_sql = "select count(card) from members where card = '" + card + "'"
+    cur.execute(ccheck_sql)
+    card_rows = cur.fetchall()
+    print(card_rows)
+
+    # for i in card_rows:
+    #     if card == i:
+    #         print("이미 등록된 사용자입니다!\n등록을 종료합니다.")
+    #         conn.close() 
+    #         return None
+        
+
     add_info = "INSERT INTO MEMBERS(CARD, NAME, ATTNUM) VALUES (?, ?, ?)"
     
     cur.execute(add_info, (card, name, 0))
