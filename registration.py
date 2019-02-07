e183ceff7c363889d009bb1fd720ad1fc3ca4f2d
Taewan Park <swimingtw@gmail.com>
2019-02-07 20:19:50 +0900

    Primary Key change in db file
    제곧내

--- registration.py
+++ registration.py
@@ -5,9 +5,8 @@ def registration():
     conn = sqlite3.connect("memberlist.db")
     cur = conn.cursor()
     create_table = """CREATE TABLE IF NOT EXISTS MEMBERS(
-        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        CARD TEXT PRIMARY KEY NOT NULL,
         NAME TEXT NOT NULL,
-        CARD TEXT NOT NULL,
         ATTNUM INTEGER NOT NULL)"""
     cur.execute(create_table)
     name = input("등록하실 이름을 입력하세요 : ")
@@ -20,15 +19,10 @@ def registration():
     print("등록하실 카드를 찍어 주세요.")
     card = nfctoid.scan_id()
 
-    add_info = "INSERT INTO MEMBERS(NAME, CARD, ATTNUM) VALUES (?, ?, ?)"
+    add_info = "INSERT INTO MEMBERS(CARD, NAME, ATTNUM) VALUES (?, ?, ?)"
     
-    cur.execute(add_info, (name, card, 0))
+    cur.execute(add_info, (card, name, 0))
     conn.commit()
-
-
-
-
-
     conn.close()
 
 registration()
