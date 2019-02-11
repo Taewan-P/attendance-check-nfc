90c966bb0760ed9ebf6a1adb6d3278dcc9861c8a
Taewan Park <swimingtw@gmail.com>
2019-02-11 19:07:08 +0900

    오류수정
    if 문 알고리즘에 오류가 있어서 수정함.

--- registration.py
+++ registration.py
@@ -4,12 +4,11 @@ import nfctoid
 def registration():
     ask = input("자람 출석부에 등록하시겠습니까? y/n\n")
     while True:
-        if ask == "N" or "n":
+        if ask == "N" or ask == "n":
             exit()
-        if ask == "Y" or "y":
+        if ask == "Y" or ask == "y":
             conn = sqlite3.connect("memberlist.db")
             cur = conn.cursor()
-    
             name = input("등록하실 이름을 입력하세요 : ")
             while True:
                 if name == "":
@@ -23,15 +22,16 @@ def registration():
             ccheck_sql = "select count(card) from members where card = '" + card + "'"
             cur.execute(ccheck_sql)
             card_rows = cur.fetchall()
-            print(card_rows)
 
             if card_rows[0][0] == 1:
                 print("이미 등록된 사용자입니다!\n등록을 종료합니다.")
                 conn.close()
+                break
             else:
-                add_info = "INSERT INTO MEMBERS(CARD, NAME, ATTNUM) VALUES (?, ?, ?)"
+                add_info = "INSERT INTO MEMBERS(CARD, NAME, ATT) VALUES (?, ?, ?)"
             
                 cur.execute(add_info, (card, name, 0))
                 conn.commit()
                 conn.close()
+                break
             
