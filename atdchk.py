d051a2350654a95870a1fe254a75c1d8953c32bd
Taewan Park <swimingtw@gmail.com>
2019-02-21 22:59:34 +0900

    Added Major Func.
    Added not to check attendance more than once in a day.
    하루에 출석이 한번 이상 찍히지 않게끔 기능을 추가했습니다.
    
    저번 커밋 메시지 한글이였는데 흑흑 ㅠㅠㅠ 이놈의 귀찮니즘

--- atdchk.py
+++ atdchk.py
@@ -1,6 +1,7 @@
 import sqlite3
 import nfctoid
 import registration
+import datetime
 
 def atdchk():
     conn = sqlite3.connect("memberlist.db")
@@ -13,9 +14,29 @@ def atdchk():
     card_rows = cur.fetchall()
     if card_rows[0][0] == 1:
         cur.execute("SELECT ATD FROM MEMBERS WHERE CARD = '" + target_card + "'")
-        atd_rows = cur.fetchall()
-        atdnum = atd_rows[0][0]
-        cur.execute("UPDATE MEMBERS SET ATD = ?, LASTCHECKED = DATETIME('NOW','LOCALTIME') WHERE CARD = ?", (atdnum+1, target_card))
-        conn.commit()
+        att_rows = cur.fetchall()
+        attnum = att_rows[0][0]
+        
+        now = datetime.datetime.now()
+        year_now = now.year
+        month_now = now.month
+        day_now = now.day
+        
+        cur.execute("SELECT LASTCHECKED FROM MEMBERS WHERE CARD = '" + target_card + "'")
+        last_date = cur.fetchall()[0][0] #last_date is a str
+        
+        converted_date = datetime.datetime.strptime(last_date.split()[0], "%Y-%m-%d").date() # This will be changed into date object.
+        year_checked = converted_date.year
+        month_checked = converted_date.month
+        day_checked = converted_date.day
+
+        if day_checked == day_now and month_checked == month_now and year_checked == year_now:
+            print("이미 오늘 출석하셨습니다.")
+            print("오늘은 " + str(year_now) + "년 " + str(month_now) +"월 " + str(day_now) + "일입니다.")
+            print("마지막으로 출석한 날짜는 " + str(year_checked) + "년 " + str(month_checked) +"월 " + str(day_checked) + "일입니다.\n")
+        else:
+            cur.execute("UPDATE MEMBERS SET ATT = ?, LASTCHECKED = DATETIME('NOW','LOCALTIME') WHERE CARD = ?", (attnum+1, target_card))
+            conn.commit()
+            print("출석이 완료되었습니다. 감사합니다!")
     else:
         registration.registration(target_card)
